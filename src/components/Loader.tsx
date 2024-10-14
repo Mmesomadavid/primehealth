26206ddd808056e2e990d7e2b8c2cbfc4c395a88
Mmesomadavid <chukwunoyelummesoma1@gmail.com>
2024-10-14 15:01:00 +0100

    added new pageloader animation

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,13 +3,13 @@ import animationData from '../assets/animations/loader.json'; // Make sure this 
 
 const Loader = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-800 w-screen overflow-hidden">
+    <div className="flex items-center justify-center h-screen bg-gray-800 w-screen overflow-hidden relative">
       <Lottie
         animationData={animationData}
         loop={true}
         style={{ width: '100vw', height: '100vh', position: 'absolute', top: 0, left: 0 }}
       />
-      <span className='flex w-4 h-[2px] rounded-full slideLoad bg-[#106fff]'></span>
+      <span className='slideLoad'></span>
     </div>
   );
 };
