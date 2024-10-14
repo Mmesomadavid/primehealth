cd0db25afce0afebcd854034ea54836939994d44
Mmesomadavid <chukwunoyelummesoma1@gmail.com>
2024-10-14 15:05:50 +0100

    made chnages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
